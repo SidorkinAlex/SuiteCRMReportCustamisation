706172eda3cefe7852d444673e0f4efb00fb8cd3
AlexSid <seedteam@yandex.ru>
2020-11-17 18:00:57 +0300

    1

---
+++ custom/modules/unified_search_modules_display.php
@@ -0,0 +1,184 @@
+<?php
+// created: 2020-11-17 17:20:01
+$unified_search_modules_display = array (
+  'AOBH_BusinessHours' => 
+  array (
+    'visible' => false,
+  ),
+  'AOD_Index' => 
+  array (
+    'visible' => false,
+  ),
+  'AOD_IndexEvent' => 
+  array (
+    'visible' => false,
+  ),
+  'AOK_Knowledge_Base_Categories' => 
+  array (
+    'visible' => false,
+  ),
+  'AOP_Case_Events' => 
+  array (
+    'visible' => false,
+  ),
+  'AOP_Case_Updates' => 
+  array (
+    'visible' => false,
+  ),
+  'AOR_Reports' => 
+  array (
+    'visible' => false,
+  ),
+  'AOR_Scheduled_Reports' => 
+  array (
+    'visible' => false,
+  ),
+  'AOS_Contracts' => 
+  array (
+    'visible' => false,
+  ),
+  'AOS_Invoices' => 
+  array (
+    'visible' => false,
+  ),
+  'AOS_PDF_Templates' => 
+  array (
+    'visible' => false,
+  ),
+  'AOS_Product_Categories' => 
+  array (
+    'visible' => false,
+  ),
+  'AOS_Products' => 
+  array (
+    'visible' => false,
+  ),
+  'AOS_Quotes' => 
+  array (
+    'visible' => false,
+  ),
+  'AOW_Processed' => 
+  array (
+    'visible' => false,
+  ),
+  'AOW_WorkFlow' => 
+  array (
+    'visible' => false,
+  ),
+  'Accounts' => 
+  array (
+    'visible' => true,
+  ),
+  'Bugs' => 
+  array (
+    'visible' => false,
+  ),
+  'Calls' => 
+  array (
+    'visible' => true,
+  ),
+  'Calls_Reschedule' => 
+  array (
+    'visible' => false,
+  ),
+  'Campaigns' => 
+  array (
+    'visible' => false,
+  ),
+  'Cases' => 
+  array (
+    'visible' => true,
+  ),
+  'Contacts' => 
+  array (
+    'visible' => true,
+  ),
+  'Documents' => 
+  array (
+    'visible' => true,
+  ),
+  'FP_Event_Locations' => 
+  array (
+    'visible' => false,
+  ),
+  'FP_events' => 
+  array (
+    'visible' => false,
+  ),
+  'Leads' => 
+  array (
+    'visible' => true,
+  ),
+  'Meetings' => 
+  array (
+    'visible' => true,
+  ),
+  'Notes' => 
+  array (
+    'visible' => true,
+  ),
+  'Opportunities' => 
+  array (
+    'visible' => true,
+  ),
+  'OutboundEmailAccounts' => 
+  array (
+    'visible' => false,
+  ),
+  'Project' => 
+  array (
+    'visible' => false,
+  ),
+  'ProjectTask' => 
+  array (
+    'visible' => false,
+  ),
+  'ProspectLists' => 
+  array (
+    'visible' => false,
+  ),
+  'Prospects' => 
+  array (
+    'visible' => false,
+  ),
+  'SurveyQuestionOptions' => 
+  array (
+    'visible' => false,
+  ),
+  'SurveyQuestionResponses' => 
+  array (
+    'visible' => false,
+  ),
+  'SurveyQuestions' => 
+  array (
+    'visible' => false,
+  ),
+  'SurveyResponses' => 
+  array (
+    'visible' => false,
+  ),
+  'Surveys' => 
+  array (
+    'visible' => false,
+  ),
+  'Tasks' => 
+  array (
+    'visible' => false,
+  ),
+  'jjwg_Address_Cache' => 
+  array (
+    'visible' => false,
+  ),
+  'jjwg_Areas' => 
+  array (
+    'visible' => false,
+  ),
+  'jjwg_Maps' => 
+  array (
+    'visible' => false,
+  ),
+  'jjwg_Markers' => 
+  array (
+    'visible' => false,
+  ),
+);
---
+++ suitecrm_version.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$suitecrm_version = '7.11.18';
+$suitecrm_timestamp = '2020-11-05 17:00:00';
