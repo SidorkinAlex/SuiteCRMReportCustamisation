fe10cdbc7628e604b70025660ee28f6e2cc84f17
SidorkinAlex <palach.orl@mail.ru>
2021-01-22 20:33:23 +0300

    add Build Cretor

---
+++ custom/Extension/modules/AOR_Fields/Ext/Vardefs/custom_sql.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seedteam
+ * Date: 24.05.20
+ * Time: 15:30
+ */
+$dictionary['AOR_Field']['fields']['select_sql']=[
+    'name' => 'select_sql',
+    'vname' => 'LBL_SELECT_SQL',
+    'type' => 'text',
+    'comment' => 'SELECT',
+    'rows' => 6,
+    'cols' => 80,
+];
